38b051b04923f48ac0f916570bbf37f12771ef30
vnavacar <98817213+vnavacar@users.noreply.github.com>
2022-12-14 12:02:48 +0100

    Class getURL

--- src/main/java/space/harbour/countries/util/GetURLfoodFacts.java
+++ src/main/java/space/harbour/countries/util/GetURLfoodFacts.java
@@ -6,9 +6,15 @@ public abstract class GetURLfoodFacts{
         String url = "https://world.openfoodfacts.org/api/v0/product/"+barcode+".json";
         return url;
     }
-    //devuelve el url para obtenor comida del pais
-    public static String getURLcountryFoodFacts(String countryPrefix){
+    //devuelve el url para obtenor nutrientes del pais
+    public static String getURLcountryFoodNutrients(String countryPrefix){
         String url = "https://"+countryPrefix+".openfoodfacts.org/cgi/nutrients.pl";
+        return url;
+    }
+
+    //devuelve la url para buscar un productor en un pais
+    public static String getURLcountryFoodProducer(String countryPrefix){
+        String url = "https://"+countryPrefix+".openfoodfacts.org/cgi/search.pl";
         return url;
     }
 }
