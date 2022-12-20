22bca11bdc7f7c469a355904751ce5fa8f4704ae
vnavacar <98817213+vnavacar@users.noreply.github.com>
2022-12-20 08:59:49 +0100

    jsonSearch

---
+++ src/main/java/space/harbour/countries/service/FoodApiService.java
@@ -0,0 +1,19 @@
+package space.harbour.countries.service;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import space.harbour.countries.domain.Country;
+
+import java.util.List;
+
+public interface FoodApiService {
+    @GET("/v3.1/all")
+    Call<List<Country>> getAllCountries();
+
+    // Be carefull because this API returns a country list even if you ask only for one
+    @GET("/v3.1/name/{name}")
+    Call<List<Country>> getCountry(@Path("name") String name);
+
+    // TODO Add as many operations as you need to call
+}
--- src/main/java/space/harbour/countries/util/GetURLfoodFacts.java
+++ src/main/java/space/harbour/countries/util/GetURLfoodFacts.java
@@ -1,5 +1,8 @@
 package space.harbour.countries.util;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 public abstract class GetURLfoodFacts{
     //devuelve url para leer json de un codigo de barra
     public static String getURLfoodFactsBarcode(int barcode){
@@ -16,5 +19,18 @@ public abstract class GetURLfoodFacts{
     public static String getURLcountryFoodProducer(String countryPrefix){
         String url = "https://"+countryPrefix+".openfoodfacts.org/cgi/search.pl";
         return url;
+    }
+
+    public String getUrlSearch(){
+
+        String url = "https://world.openfoodfacts.org/api/v2/search";
+        //junto a esto hay que enviar un json con los parametros de busqueda
+        return url;
+    }
+    public JsonObject getJsonSearch(){
+        Gson g = new Gson();
+        JsonObject json = g.fromJson("categories_tags_en=pizza", JsonObject.class);
+        //json con los parametros de busqueda
+        return json;
     }
 }
